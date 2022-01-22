7573fb6744955f2758b010ab09eaa2fdc42cfdaf
TurkRagnar <48673608+turkragnar@users.noreply.github.com>
2022-01-22 23:35:22 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+1.
+--------------------------------------------------------------------
+function getCookie(cName) {
+  const name = cName + "=";
+  const cDecoded = decodeURIComponent(document.cookie);
+  const cArr = cDecoded.split('; ');
+  let res;
+  cArr.forEach(val => {
+    if (val.indexOf(name) === 0) res = val.substring(name.length);
+  })
+  return res
+};
+
+let CopyCookie= getCookie('loginToken');
+console.log(CopyCookie);
+
+
+
+2.
+--------------------------------------------------------------------
+document.cookie="loginToken=//CopyCookie//;"
